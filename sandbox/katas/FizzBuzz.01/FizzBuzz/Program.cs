e745bed3becee5c747657db6ef10ad1e8e5f0c7b
LKrivankova <krivankovaelenka@gmail.com>
2024-10-06 18:09:10 +0200

    FizzBuzz assignment - addition

--- sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
@@ -6,6 +6,6 @@ fizzBuzz.CountTo(20);
 
 Console.WriteLine("=== Palo's solution ===");
 
-FizzBuzz_solution_Palo fizzBuzz_Solution_Palo = new FizzBuzz_solution_Palo();
+var fizzBuzz_Solution_Palo = new FizzBuzz_solution_Palo();
 
 fizzBuzz_Solution_Palo.CountTo(30);
